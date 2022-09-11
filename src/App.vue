5c7d0b4c917de170bd6e9d2427db9dd8b7342481
Anton Zagrebelny <anton@stigg.io>
2022-09-11 09:52:00 +0300

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { applyReactInVue } from "veaury";
+import { StiggProvider, Paywall } from "@stigg/react-sdk";
+import type { Plan } from "@stigg/js-client-sdk";
+
+export default {
+  components: {
+    StiggProvider: applyReactInVue(StiggProvider),
+    Paywall: applyReactInVue(Paywall),
+  },
+  methods: {
+    onPlanSelected: ({ plan }: { plan: Plan }) => {
+      console.log(`Selected plan: ${plan.displayName}`);
+    },
+  },
+  setup() {
+    return {
+      apiKey:
+        "YOUR-CLIENT-API-KEY",
+      theme: {
+        layout: {
+          planMinWidth: "320px",
+        },
+      },
+    };
+  },
+};
+</script>
+
+<template>
+  <StiggProvider :apiKey="apiKey" :theme="theme">
+    <Paywall :onPlanSelected="onPlanSelected" />
+  </StiggProvider>
+</template>
